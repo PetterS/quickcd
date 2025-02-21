cdf6b614d9b8447611659baa68de1c996dfbd3d6
Petter Strandmark <petter.strandmark@gmail.com>
2025-02-21 22:10:26 +0100

    Introduce DB migrations

--- quickcd-store.cpp
+++ quickcd-store.cpp
@@ -15,11 +15,61 @@ class Db {
 		}
 		sqlite3_busy_timeout(db, 10000);
 	}
+
 	~Db() {
 		sqlite3_close(db);
 	}
+
+	void run(const char* sql,
+	         int (*callback)(void*, int, char**, char**) = nullptr,
+	         void* data = nullptr) {
+		char* err = nullptr;
+		int result = sqlite3_exec(db, sql, callback, data, &err);
+		if (result != 0) {
+			if (err != nullptr) {
+				cerr << err << endl;
+				sqlite3_free(err);
+			}
+			cerr << "FAIL: " << sql << "\n";
+			std::exit(2);
+		}
+	}
+
 	sqlite3* db;
 };
+
+static int set_int(void* data, int, char** result, char**) {
+	int* number = (int*)data;
+	*number = std::atoi(result[0]);
+	return 0;
+}
+
+static void migrate(Db* db) {
+	int user_version = 0;
+	db->run("pragma user_version;", set_int, &user_version);
+	switch (user_version) {
+		default:
+			cerr << "Unexpected user_version!\n";
+			std::exit(3);
+
+		case 0:
+			db->run(
+			    "CREATE TABLE IF NOT EXISTS dirs (name text PRIMARY KEY NOT NULL, count integer NOT "
+			    "NULL);\n"
+			    "CREATE TABLE IF NOT EXISTS commands (timestamp datetime default current_timestamp,"
+			    "                                     command text,"
+			    "                                     directory text,"
+			    "                                     return_value integer,"
+			    "                                     time real"
+			    ");\n"
+			    "PRAGMA journal_mode=WAL;\n"
+			    "PRAGMA user_version = 1;");
+			cerr << "DB migrated to version 1\n";
+		case 1:
+		    /*current version*/;
+	}
+	db->run("pragma user_version;", set_int, &user_version);
+}
 
 int main(int nargs, char* args[]) {
 	if (nargs != 3 && nargs != 6) {
@@ -28,36 +78,21 @@ int main(int nargs, char* args[]) {
 		     << " <dbfile> <directory to store> <command> <return val> <time>\n";
 		return 1;
 	}
-	const Db db(args[1]);
+	Db db(args[1]);
+	migrate(&db);
 
 	char* sql = sqlite3_mprintf(
-	    "CREATE TABLE IF NOT EXISTS dirs (name text PRIMARY KEY NOT NULL, count integer NOT NULL);"
 	    "BEGIN;"
 	    "    INSERT INTO dirs (name, count) VALUES (%Q, 0) ON CONFLICT DO NOTHING;"
 	    "    UPDATE dirs SET count = count + 1 WHERE name = %Q;"
 	    "COMMIT;",
 	    args[2],
 	    args[2]);
-	char* err = nullptr;
-	int result = sqlite3_exec(db.db, sql, nullptr, nullptr, &err);
+	db.run(sql);
 	sqlite3_free(sql);
-
-	if (result != 0) {
-		if (err != nullptr) {
-			cerr << err << endl;
-			sqlite3_free(err);
-		}
-		return 3;
-	}
 
 	if (nargs == 6) {
 		char* sql = sqlite3_mprintf(
-		    "CREATE TABLE IF NOT EXISTS commands (timestamp datetime default current_timestamp,"
-		    "                                     command text,"
-		    "                                     directory text,"
-		    "                                     return_value integer,"
-		    "                                     time real"
-		    ");"
 		    "BEGIN;"
 		    "INSERT INTO commands (command, directory, return_value, time) VALUES (%Q, %Q, "
 		    "%Q, %Q);"
@@ -66,17 +101,8 @@ int main(int nargs, char* args[]) {
 		    args[2],
 		    args[4],
 		    args[5]);
-		char* err = nullptr;
-		int result = sqlite3_exec(db.db, sql, nullptr, nullptr, &err);
+		db.run(sql);
 		sqlite3_free(sql);
-
-		if (result != 0) {
-			if (err != nullptr) {
-				cerr << err << endl;
-				sqlite3_free(err);
-			}
-			return 3;
-		}
 	}
 
 	return 0;
